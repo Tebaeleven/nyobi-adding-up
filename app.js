3b32b058f80272ad52bbc7e8e5ebdb9b28dd88a7
nano <nano@nanonoMacBook-Air.local>
2023-05-21 15:54:16 +0900

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+const fs = require('node:fs'); //FileSystemモジュール呼び出し
+const readline = require('node:readline'); //ファイルを一行ずつ読み込むためのモジュール
+const rs = fs.createReadStream('./popu-pref.csv');  //popu-pref.csvファイルから、ファイルを読み込む Streamを生成
+const rl = readline.createInterface({ input: rs }); //readlineオブジェクトのinputとして設定,rlオブジェクトを作成
+rl.on('line', lineString => {
+    console.log(lineString);
+});
